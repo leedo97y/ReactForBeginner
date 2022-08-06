a6221e208a3492095b01da7befe071ac177c7834
leedo97y <dlehdus1433@gmail.com>
2022-08-06 22:11:12 +0900

    강의 코드랑 분리하기 위해 file 하나 생성

---
+++ conversionApp.jsx
@@ -0,0 +1,81 @@
+const App = () => {
+  const [amount, setAmount] = React.useState(0);
+  const [flipped, setFlipped] = React.useState(false);
+  const [hide, setHide] = React.useState(false);
+
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const reset = () => setAmount(0);
+
+  const onFlip = () => {
+    reset();
+    setFlipped((current) => !current);
+  };
+  // if flipped is false => true
+  // if flipped is true => false
+
+  const onSwitch = () => {
+    reset();
+    setHide((current) => !current);
+  };
+
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <div hidden={hide}>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          id="minutes"
+          value={flipped ? amount * 60 : amount}
+          placeholder="Minutes"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === true}
+        />
+      </div>
+      <div hidden={hide}>
+        <label htmlFor="hours">Hours</label>
+        <input
+          id="hours"
+          value={flipped ? amount : Math.round(amount / 60)}
+          // if flipped = minutes
+          // if !flipped = Math.round(minutes / 60)
+          placeholder="Hours"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === false}
+        />
+      </div>
+      <div hidden={!hide}>
+        <label htmlFor="miles">Miles</label>
+        <input
+          id="miles"
+          value={flipped ? Math.round(amount * 0.6214 * 1000) / 1000 : amount}
+          placeholder="Miles"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === true}
+        />
+      </div>
+      <div hidden={!hide}>
+        <label htmlFor="kilometers">Kilmeters</label>
+        <input
+          id="kilometers"
+          value={flipped ? amount : Math.round((amount / 0.6214) * 1000) / 1000}
+          placeholder="Kilometers"
+          type="number"
+          onChange={onChange}
+          disabled={flipped === false}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>Flip</button>
+      <button onClick={onSwitch}>Switch</button>
+    </div>
+  );
+};
+
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
