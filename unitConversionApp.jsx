1e34c40af965225d05de7a76858b61172521f7b9
leedo97y <dlehdus1433@gmail.com>
2022-08-08 17:07:18 +0900

    move prac file in prac folder, review all concept

--- unitConversionApp.jsx
+++ unitConversionApp.jsx
@@ -1,8 +1,9 @@
 // default value를 설정해줄 수 있음
 // fontSize가 존재하지 않는다면 default value로 설정됨
-function Btn({ text, fontSize = 15 }) {
+function Btn({ text, fontSize = 15, onClick }) {
   return (
     <button
+      onClick={onClick}
       style={{
         backgroundColor: "tomato",
         color: "white",
@@ -21,14 +22,22 @@ Btn.propTypes = {
   text: PropTypes.string,
   fontSize: PropTypes.number,
 };
+// propTypes를 지정하기 위해선 우선 패키지?를 설치해야한다.
 // propTypes를 지정해주면, type을 틀렸을때 경고 문구가 뜬다.
 // 어디서 오류가 났는지 파악하기 쉬움
 
+const MemorizeButton = React.memo(Btn);
+// 버튼의 상태를 저장해주어 바뀌지 않은 버튼의 re-rendering을 피하게 해준다.
+
 const App = () => {
+  const [textValue, setTextValue] = React.useState("Save Changes");
+  const changeText = () => {
+    setTextValue("Revert Changes");
+  };
   return (
     <div>
-      <Btn text="Save Changes" fontSize={18} />
-      <Btn text="Continue" />
+      <MemorizeButton text={textValue} fontSize={18} onClick={changeText} />
+      <MemorizeButton text="Continue" />
     </div>
   );
 };
